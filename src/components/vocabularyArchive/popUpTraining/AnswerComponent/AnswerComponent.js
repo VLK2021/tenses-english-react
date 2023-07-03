2342941a566b2a261957ee78d19b13b96128c930
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-07-04 01:08:17 +0300

    add comments in cod

--- src/components/vocabularyArchive/popUpTraining/AnswerComponent/AnswerComponent.js
+++ src/components/vocabularyArchive/popUpTraining/AnswerComponent/AnswerComponent.js
@@ -1,86 +1,18 @@
-// import React, {useCallback, useEffect} from 'react';
-// import {useForm} from "react-hook-form";
-//
-// import './AnswerComponentStyle.css';
-//
-//
-// const AnswerComponent = ({answerArray, infinitive}) => {
-//     const {register, setValue} = useForm();
-//
-//
-//     //обнуляю інпут
-//     useEffect(() => {
-//         setValue('radioOption', '');
-//     }, [infinitive]);
-//
-//     //перемішую масив щоб кожен раз міняти розташування правильного слова
-//     const shuffleArray = useCallback((array) => {
-//         const newArray = [...array];
-//         for (let i = newArray.length - 1; i > 0; i--) {
-//             const j = Math.floor(Math.random() * (i + 1));
-//             [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-//         }
-//         return newArray;
-//     }, []);
-//
-//     //отримую перемішаний новий масив для рендеру
-//     const shuffledAnswerArray = shuffleArray(answerArray);
-//
-//     const submit = (data) => {
-//         if(data.radioOption === infinitive) {
-//             // alert('good!!!');
-//             console.log('good');
-//         } else {
-//             // alert('bad!!!');
-//             console.log('bad');
-//         }
-//     };
-//
-//     const handleRadioChange = (e) => {
-//         const selectedOption = e.target.value;
-//         setValue('radioOption', selectedOption);
-//         submit({radioOption: selectedOption});
-//     };
-//
-//
-//     return (
-//         <main className={'width answerComponent marginTop'}>
-//             <form className={'flex'}>
-//                 {
-//                     shuffledAnswerArray.length &&
-//                     shuffledAnswerArray.map((obj, index) =>
-//                         <div key={index} className={`width answersComponent-block `}>
-//                             <div className={'answersComponent-block-input'}>
-//                                 <input type="radio" value={obj}
-//                                        {...register(`radioOption`)}
-//                                        onChange={handleRadioChange}
-//                                 />
-//                             </div>
-//
-//                             <div className={`answersComponent-block-name`}>{obj}</div>
-//                         </div>
-//                     )
-//                 }
-//             </form>
-//         </main>
-//     );
-// };
-//
-// export {AnswerComponent};
-
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import './AnswerComponentStyle.css';
+
 
 const AnswerComponent = ({ answerArray, infinitive }) => {
     const { register, setValue } = useForm();
 
     const [selectedOption, setSelectedOption] = useState('');
     const [changeColor, setChangeColor] = useState({});
-    console.log(changeColor);
+
     const [shuffledAnswerArray, setShuffledAnswerArray] = useState([]);
 
+    //обнуляємо та наповнюємо стейти початковими значеннями
     useEffect(() => {
         setValue('radioOption', '');
         setSelectedOption('');
@@ -88,6 +20,7 @@ const AnswerComponent = ({ answerArray, infinitive }) => {
         setShuffledAnswerArray(shuffleArray(answerArray));
     }, [answerArray, infinitive, setValue]);
 
+    //встановлюємо колір для слова
     useEffect(() => {
         setChangeColor({});
         const color = selectedOption === infinitive ? 'green' : 'red';
@@ -96,6 +29,7 @@ const AnswerComponent = ({ answerArray, infinitive }) => {
         }
     }, [selectedOption, infinitive]);
 
+    //перемішуємо масив слів
     const shuffleArray = (array) => {
         const newArray = [...array];
         for (let i = newArray.length - 1; i > 0; i--) {
