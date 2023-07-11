fe3af0109426d159facb95b556ed6b4604bac87a
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-07-11 23:20:39 +0300

    commit

--- src/constants/vocabulary/vocabularyForFamily.js
+++ src/constants/vocabulary/vocabularyForFamily.js
@@ -3,222 +3,550 @@ export const vocabularyForFamily = [
         id: 'F1',
         infinitive: 'mother',
         transcriptionInf: '[ˈmʌðər]',
-        translation: 'мама'
+        translation: 'мама',
+        incorrect_answers: [
+            "son-in-law",
+            "daughter-in-law",
+            "sister-in-law"
+        ]
     },
     {
         id: 'F2',
         infinitive: 'father',
         transcriptionInf: '[ˈfɑːðər]',
-        translation: 'батько'
+        translation: 'батько',
+        incorrect_answers: [
+            "generation",
+            "great-grandchild",
+            "marriage"
+        ]
     },
     {
         id: 'F3',
         infinitive: 'sister',
         transcriptionInf: '[ˈsɪstər]',
-        translation: 'сестра'
+        translation: 'сестра',
+        incorrect_answers: [
+            "descendant",
+            "stepfather",
+            "stepbrother"
+        ]
     },
     {
         id: 'F4',
         infinitive: 'brother',
         transcriptionInf: '[ˈbrʌðər]',
-        translation: 'брат'
+        translation: 'брат',
+        incorrect_answers: [
+            "father",
+            "mother",
+            "son"
+        ]
     },
     {
         id: 'F5',
         infinitive: 'son',
         transcriptionInf: '[sʌn]',
-        translation: 'син'
+        translation: 'син',
+        incorrect_answers: [
+            "grandmother",
+            "daughter",
+            "father"
+        ]
     },
     {
         id: 'F6',
         infinitive: 'daughter',
         transcriptionInf: '[ˈdɔːtər]',
-        translation: 'дочка'
+        translation: 'дочка',
+        incorrect_answers: [
+            "husband",
+            "niece",
+            "cousin"
+        ]
     },
     {
         id: 'F7',
         infinitive: 'grandmother',
         transcriptionInf: '[ˈɡræn(d)ˌmʌðər]',
-        translation: 'бабуся'
+        translation: 'бабуся',
+        incorrect_answers: [
+            "daughter",
+            "grandfather",
+            "stepbrother"
+        ]
     },
     {
         id: 'F8',
         infinitive: 'grandfather',
         transcriptionInf: '[ˈɡræn(d)ˌfɑːðər]',
-        translation: 'дідусь'
+        translation: 'дідусь',
+        incorrect_answers: [
+            "descendant",
+            "ancestor",
+            "relatives"
+        ]
     },
     {
         id: 'F9',
         infinitive: 'aunt',
         transcriptionInf: '[ænt]',
-        translation: 'тітка'
+        translation: 'тітка',
+        incorrect_answers: [
+            "family",
+            "husband",
+            "cousin"
+        ]
     },
     {
         id: 'F10',
         infinitive: 'uncle',
         transcriptionInf: '[ˈʌŋk(ə)l]',
-        translation: 'дядько'
+        translation: 'дядько',
+        incorrect_answers: [
+            "nephew",
+            "spouse",
+            "parents"
+        ]
     },
     {
         id: 'F11',
         infinitive: 'cousin',
         transcriptionInf: '[ˈkʌzn]',
-        translation: 'двоюрідний брат/сестра'
+        translation: 'двоюрідний брат/сестра',
+        incorrect_answers: [
+            "aunt",
+            "daughter",
+            "sister"
+        ]
     },
     {
         id: 'F12',
         infinitive: 'niece',
         transcriptionInf: '[niːs]',
-        translation: 'племінниця'
+        translation: 'племінниця',
+        incorrect_answers: [
+            "stepsister",
+            "descendant",
+            "niece-in-law"
+        ]
     },
     {
         id: 'F13',
         infinitive: 'nephew',
         transcriptionInf: '[ˈnefjuː]',
-        translation: 'племінник'
+        translation: 'племінник',
+        incorrect_answers: [
+            "sibling",
+            "adopted son",
+            "adopted child"
+        ]
     },
     {
         id: 'F14',
         infinitive: 'husband',
         transcriptionInf: '[ˈhʌzbənd]',
-        translation: 'чоловік (одружений)'
+        translation: 'чоловік (одружений)',
+        incorrect_answers: [
+            "marriage",
+            "adopted child",
+            "generation"
+        ]
     },
     {
         id: 'F15',
         infinitive: 'wife',
         transcriptionInf: '[waɪf]',
-        translation: 'дружина'
+        translation: 'дружина',
+        incorrect_answers: [
+            "family",
+            "stepmother",
+            "stepfather"
+        ]
     },
     {
         id: 'F16',
         infinitive: 'spouse',
         transcriptionInf: '[spaʊs]',
-        translation: 'подружжя'
+        translation: 'подружжя',
+        incorrect_answers: [
+            "son-in-law",
+            "daughter-in-law",
+            "sister-in-law"
+        ]
     },
     {
         id: 'F17',
         infinitive: 'parents',
         transcriptionInf: '[ˈperənts]',
-        translation: 'батьки'
+        translation: 'батьки',
+        incorrect_answers: [
+            "sibling",
+            "adopted son",
+            "adopted child"
+        ]
     },
     {
         id: 'F18',
         infinitive: 'children',
         transcriptionInf: '[ˈtʃɪldrən]',
-        translation: 'діти'
+        translation: 'діти',
+        incorrect_answers: [
+            "generation",
+            "great-grandchild",
+            "marriage"
+        ]
     },
     {
         id: 'F19',
         infinitive: 'family',
         transcriptionInf: '[ˈfæm(ə)li]',
-        translation: 'сімя'
+        translation: 'сімя',
+        incorrect_answers: [
+            "relatives",
+            "stepsister",
+            "great-grandchild"
+        ]
     },
     {
         id: 'F20',
         infinitive: 'relatives',
         transcriptionInf: '[ˈrelətɪvz]',
-        translation: 'родичі'
+        translation: 'родичі',
+        incorrect_answers: [
+            "Tongue",
+            "Artery",
+            "Bone"
+        ]
     },
     {
         id: 'F21',
         infinitive: 'ancestor',
         transcriptionInf: '[ˈænsestər]',
-        translation: 'предок'
+        translation: 'предок',
+        incorrect_answers: [
+            "Mouth",
+            "calm",
+            "Chin"
+        ]
     },
     {
         id: 'F22',
         infinitive: 'descendant',
         transcriptionInf: '[dɪˈsendənt]',
-        translation: 'нащадок'
+        translation: 'нащадок',
+        incorrect_answers: [
+            "Nose",
+            "Forehead",
+            "Ankle"
+        ]
     },
     {
         id: 'F23',
         infinitive: 'stepmother',
         transcriptionInf: '[ˈstɛpˌmʌðər]',
-        translation: 'мачуха'
+        translation: 'мачуха',
+        incorrect_answers: [
+            "Elbow",
+            "Eyebrow",
+            "Shoulder"
+        ]
     },
     {
         id: 'F24',
         infinitive: 'stepfather',
         transcriptionInf: '[ˈstɛpˌfɑːðər]',
-        translation: 'отчим'
+        translation: 'отчим',
+        incorrect_answers: [
+            "Head",
+            "Eyelid",
+            "Chin"
+        ]
     },
     {
         id: 'F25',
         infinitive: 'stepbrother',
         transcriptionInf: '[ˈstɛpˌbrʌðər]',
-        translation: 'сводний брат'
+        translation: 'сводний брат',
+        incorrect_answers: [
+            "Stomach ",
+            "Hip",
+            "Palm "
+        ]
     },
     {
         id: 'F26',
         infinitive: 'stepsister',
         transcriptionInf: '[ˈstɛpˌsɪstər]',
-        translation: 'сводна сестра'
+        translation: 'сводна сестра',
+        incorrect_answers: [
+            "successful",
+            "grateful",
+            "sincere"
+        ]
     },
     {
         id: 'F27',
         infinitive: 'father-in-law',
         transcriptionInf: '[ˈfɑːðər ɪn ˌlɔː]',
-        translation: 'тесть (батько партнера)'
+        translation: 'тесть (батько партнера)',
+        incorrect_answers: [
+            "brave",
+            "calm",
+            "charming"
+        ]
     },
     {
         id: 'F28',
         infinitive: 'mother-in-law',
         transcriptionInf: '[ˈmʌðər ɪn ˌlɔː]',
-        translation: 'свекруха (мати партнера)'
+        translation: 'свекруха (мати партнера)',
+        incorrect_answers: [
+            "algorithm",
+            "query",
+            "binary"
+        ]
     },
     {
         id: 'F29',
         infinitive: 'brother-in-law',
         transcriptionInf: '[ˈbrʌðər ɪn ˌlɔː]',
-        translation: 'шурин (брат партнера)'
+        translation: 'шурин (брат партнера)',
+        incorrect_answers: [
+            "slowly",
+            "suddenly",
+            "sincere"
+        ]
     },
     {
         id: 'F30',
         infinitive: 'sister-in-law',
         transcriptionInf: '[ˈsɪstər ɪn ˌlɔː]',
-        translation: 'сестра партнера'
+        translation: 'сестра партнера',
+        incorrect_answers: [
+            "hello",
+            "brave",
+            "apple"
+        ]
     },
     {
         id: 'F31',
         infinitive: 'son-in-law',
         transcriptionInf: '[sʌn ɪn ˌlɔː]',
-        translation: 'зять'
+        translation: 'зять',
+        incorrect_answers: [
+            "gently",
+            "calmly",
+            "firmly"
+        ]
     },
     {
         id: 'F32',
         infinitive: 'daughter-in-law',
         transcriptionInf: '[ˈdɔːtər ɪn ˌlɔː]',
-        translation: 'невістка'
+        translation: 'невістка',
+        incorrect_answers: [
+            "honest",
+            "generous",
+            "charming"
+        ]
     },
     {
         id: 'F33',
         infinitive: 'nephew-in-law',
         transcriptionInf: '[ˈnefjuː ɪn ˌlɔː]',
-        translation: 'племінник (чоловік племінниці)'
+        translation: 'племінник (чоловік племінниці)',
+        incorrect_answers: [
+            "careful",
+            "sincere",
+            "firmly"
+        ]
     },
     {
         id: 'F34',
         infinitive: 'niece-in-law',
         transcriptionInf: '[niːs ɪn ˌlɔː]',
-        translation: 'племінниця (дружина племінника)'
+        translation: 'племінниця (дружина племінника)',
+        incorrect_answers: [
+            "beautiful",
+            "brave",
+            "clever"
+        ]
     },
     {
         id: 'F35',
         infinitive: 'godmother',
         transcriptionInf: '[ˈɡɒdmʌðər]',
-        translation: 'хрещена мати'
+        translation: 'хрещена мати',
+        incorrect_answers: [
+            "adware",
+            "analysis",
+            "byte"
+        ]
     },
     {
         id: 'F36',
         infinitive: 'godfather',
         transcriptionInf: '[ˈɡɒdfɑːðər]',
-        translation: 'хрещений батько'
+        translation: 'хрещений батько',
+        incorrect_answers: [
+            "algorithm",
+            "query",
+            "binary"
+        ]
     },
     {
         id: 'F37',
         infinitive: 'godchild',
         transcriptionInf: '[ˈɡɒdʧaɪld]',
-        translation: 'хрещенець/хрещениця'
+        translation: 'хрещенець/хрещениця',
+        incorrect_answers: [
+            "account",
+            "variable",
+            "encryption"
+        ]
+    },
+    {
+        id: 'F38',
+        infinitive: 'twins',
+        transcriptionInf: '[twɪnz]',
+        translation: 'близнюки',
+        incorrect_answers: [
+            "brave",
+            "generous",
+            "wise"
+        ]
+    },
+    {
+        id: 'F39',
+        infinitive: 'grandson',
+        transcriptionInf: '[ˈɡrænˌsʌn]',
+        translation: 'онук',
+        incorrect_answers: [
+            "smart",
+            "loving",
+            "optimistic"
+        ]
+    },
+    {
+        id: 'F40',
+        infinitive: 'granddaughter',
+        transcriptionInf: '[ˈɡrænˌdɔːtər]',
+        translation: 'онука',
+        incorrect_answers: [
+            "clever",
+            "happy",
+            "bug"
+        ]
+    },
+    {
+        id: 'F41',
+        infinitive: 'grandchildren',
+        transcriptionInf: '[ˈɡræn(d)ˌtʃɪldrən]',
+        translation: 'онуки',
+        incorrect_answers: [
+            "generation",
+            "adopted child",
+            "children"
+        ]
+    },
+    {
+        id: 'F42',
+        infinitive: 'great-grandfather',
+        transcriptionInf: '[ɡreɪtˈɡrændˌfɑːðər]',
+        translation: 'прадід',
+        incorrect_answers: [
+            "spouse",
+            "wife",
+            "relatives"
+        ]
+    },
+    {
+        id: 'F43',
+        infinitive: 'great-grandmother',
+        transcriptionInf: '[ɡreɪtˈɡrændˌmʌðər]',
+        translation: 'прабабуся',
+        incorrect_answers: [
+            "successful",
+            "grateful",
+            "sincere"
+        ]
+    },
+    {
+        id: 'F44',
+        infinitive: 'great-grandchild',
+        transcriptionInf: '[ɡreɪtˈɡrændˌtʃaɪld]',
+        translation: 'правнук',
+        incorrect_answers: [
+            "successful",
+            "grateful",
+            "sincere"
+        ]
+    },
+    {
+        id: 'F45',
+        infinitive: 'sibling',
+        transcriptionInf: '[ˈsɪblɪŋ]',
+        translation: 'брат чи сестра',
+        incorrect_answers: [
+            "grapefruit",
+            "son",
+            "generally"
+        ]
+    },
+    {
+        id: 'F46',
+        infinitive: 'generation',
+        transcriptionInf: '[ˌdʒɛnəˈreɪʃən]',
+        translation: 'покоління',
+        incorrect_answers: [
+            "father",
+            "son",
+            "daughter"
+        ]
+    },
+    {
+        id: 'F47',
+        infinitive: 'adopted son',
+        transcriptionInf: '[əˈdɑptɪd sʌn]',
+        translation: 'прийомний син',
+        incorrect_answers: [
+            "patiently",
+            "happy",
+            "wise"
+        ]
+    },
+    {
+        id: 'F48',
+        infinitive: 'adopted daughter',
+        transcriptionInf: '[əˈdɑptɪd ˈdɔtər]',
+        translation: 'прийомна дочка',
+        incorrect_answers: [
+            "optimistic",
+            "talented",
+            "loving"
+        ]
+    },
+    {
+        id: 'F49',
+        infinitive: 'adopted child',
+        transcriptionInf: '[əˈdɑptɪd tʃaɪld]',
+        translation: 'усиновлена дитина',
+        incorrect_answers: [
+            "humble",
+            "successful",
+            "firmly"
+        ]
+    },
+    {
+        id: 'F50',
+        infinitive: 'marriage',
+        transcriptionInf: '[ˈmærɪdʒ]',
+        translation: 'шлюб',
+        incorrect_answers: [
+            "curious",
+            "kind",
+            "funny"
+        ]
     },
 ];
