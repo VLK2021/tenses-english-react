70275426d1792c31ed664a3aa2a7ed6f2e517cac
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-10-27 21:28:36 +0200

    add home component

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,43 +1,52 @@
 import React from 'react';
-import {Outlet} from 'react-router-dom';
+import {Outlet, useLocation} from 'react-router-dom';
 
 import './LayoutStyle.css';
 import {menu} from '../constants';
 import Button from "../components/Button/Button";
-import {Footer, Header} from "../components";
+import {Footer, Header, HomeComponent} from "../components";
 
 
 const Layout = () => {
     const secondNumberSlice = menu.length / 2;
+    const location = useLocation();
+
 
 
     return (
         <main className={'main width flexDirectionColumn'}>
-            <header className={'header-block'}>
-                <Header/>
-            </header>
+            {location.pathname !== '/' ?
+                    <section className={'container'}>
+                        <header className={'header-block'}>
+                            <Header/>
+                        </header>
 
-            <section className={'width layout'}>
-                <aside className={'layout-left'}>
-                    {
-                        menu.slice(0, secondNumberSlice).map(obj => <Button key={obj.id} obj={obj}/>)
-                    }
-                </aside>
+                        <section className={'width layout'}>
+                            <aside className={'layout-left'}>
+                                {
+                                    menu.slice(0, secondNumberSlice).map(obj => <Button key={obj.id} obj={obj}/>)
+                                }
+                            </aside>
 
-                <article className={'outlet'}>
-                    <Outlet/>
-                </article>
+                            <article className={'outlet'}>
+                                <Outlet/>
+                            </article>
 
-                <aside className={'layout-right'}>
-                    {
-                        menu.slice(secondNumberSlice, menu.length).map(obj => <Button key={obj.id} obj={obj}/>)
-                    }
-                </aside>
-            </section>
+                            <aside className={'layout-right'}>
+                                {
+                                    menu.slice(secondNumberSlice, menu.length).map(obj => <Button key={obj.id}
+                                                                                                  obj={obj}/>)
+                                }
+                            </aside>
+                        </section>
 
-            <footer className={'width'}>
-                <Footer/>
-            </footer>
+                        <footer className={'width'}>
+                            <Footer/>
+                        </footer>
+                    </section>
+                    :
+                    <section className={'maineHomeComponentBlock'}><HomeComponent/></section>
+            }
         </main>
     );
 };
